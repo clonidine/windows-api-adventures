ef11412e79ff9c68cc8ded2d00b2692f619f5046
miggx <migxp@proton.me>
2023-09-05 17:19:34 -0300

    Refactoring

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -6,9 +6,9 @@ use windows::{
 const ACCESS_RIGHTS: u32 = 0x1F0FFF;
 
 pub fn open_process(process_id: u32) -> Result<()> {
-    let process = unsafe { OpenProcess(PROCESS_ACCESS_RIGHTS(ACCESS_RIGHTS), false, process_id) };
+    let handle = unsafe { OpenProcess(PROCESS_ACCESS_RIGHTS(ACCESS_RIGHTS), false, process_id) };
 
-    match process {
+    match handle {
         Ok(process) => {
             println!("Process handle: {:?}", process);
         }
